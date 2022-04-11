3425259e781d315d612d472123e00d42549ddb35
JuEnPeHa <jeph@nearlearning.app>
2022-04-10 22:11:38 -0500

    cndsncdsocmds

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         let result = 2 + 2;
+//         assert_eq!(result, 4);
+//     }
+// }
+
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{near_bindgen,log};
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, Default)]
+pub struct Counter {
+    value: u64,
+}
+
+#[near_bindgen]
+impl Counter {
+    #[init]
+    pub fn new(value: u64) -> Self {
+        log!("Custom counter initialization!");
+        Self { value }
+    }
+    pub fn inc(&mut self) {
+        self.value += 1;
+    }
+
+    pub fn get(&self) -> u64 {
+        self.value
+    }
+}
